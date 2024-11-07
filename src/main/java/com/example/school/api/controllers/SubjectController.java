37da760b038abe701536a5b1509bd284afd5f028
ivanovad <ivanov@o-code.ru>
2024-11-07 18:50:02 +0400

    [07-11-2024] Переделка для лабы по микросервисам

--- src/main/java/com/example/school/api/controllers/SubjectController.java
+++ src/main/java/com/example/school/api/controllers/SubjectController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping({"/api/subjects"})
 @Tag(name = "Контроллер для предметов")
 public class SubjectController extends BaseController<SubjectEntity, SubjectPojo> {
