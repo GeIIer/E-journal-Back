70cf408d1559feb6a4b9887498d8cf1f809eb3bb
Geller <artiom.ivanov.13112001@yandex.ru>
2024-12-15 19:05:30 +0400

    15-12-2024 - рефакторинг

--- src/main/java/com/example/school/mongo/models/Subject.java
+++
@@ -1,9 +0,0 @@
-package com.example.school.mongo.models;
-
-import lombok.Data;
-
-@Data
-public class Subject {
-    private String name;
-    private String description;
-}
--- src/main/java/com/example/school/redis/services/MessageService.java
+++
@@ -1,5 +0,0 @@
-package com.example.school.redis.services;
-
-public interface MessageService {
-    Long publishMessage(String msg);
-}
