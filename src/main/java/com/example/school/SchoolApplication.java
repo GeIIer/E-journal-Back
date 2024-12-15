dad7fd90a5c46474b5eeaca170b96bd83ee7837a
Geller <artiom.ivanov.13112001@yandex.ru>
2024-12-15 19:07:26 +0400

    15-12-2024 - рефакторинг

--- src/main/java/com/example/school/SchoolApplication.java
+++ src/main/java/com/example/school/SchoolApplication.java
@@ -1,36 +1,11 @@
 package com.example.school;
 
-import com.example.school.redis.services.Receiver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class SchoolApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SchoolApplication.class, args);
-    }
-
-    @Bean
-    public ChannelTopic topic() {
-        return new ChannelTopic("pubsub:my topic");
-    }
-
-    @Bean
-    public MessageListenerAdapter messagelisterner() {
-        return new MessageListenerAdapter(new Receiver());
-    }
-
-    @Bean
-    public RedisMessageListenerContainer getContainer(RedisConnectionFactory redisConnectionFactory) {
-        RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
-        redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory);
-        redisMessageListenerContainer.addMessageListener(messagelisterner(), topic());
-        return redisMessageListenerContainer;
     }
 }
